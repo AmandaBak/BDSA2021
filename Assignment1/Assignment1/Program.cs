f0a0e303d5c74ca18f552eac4358b26e7bb5726d
ambc <ambc@itu.dk>
2021-09-22 14:59:27 +0200

    Implemented error handling.

--- Assignment1/Assignment1/Program.cs
+++ Assignment1/Assignment1/Program.cs
@@ -14,10 +14,14 @@ namespace Assignment1
         }
 
         public static bool IsLeapYear(int year) {
+            if (year < 1582) {
+                Console.WriteLine("Only years from 1582 and onwards");
+                return false;
+            }
             if (year % 400 == 0) return true; 
-            else if (year % 100 == 0) return false;
-            else if (year % 4 == 0) return true;
-            else return false;
+            if (year % 100 == 0) return false;
+            if (year % 4 == 0) return true;
+            return false;
         }
     }
 }
